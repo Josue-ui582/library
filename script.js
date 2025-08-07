62aa57c123f5afcb090de26e3931dd3960e090d3
Josue <hounhouijosue1@gmail.com>
2025-08-07 14:51:57 +0100

    display book

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const bookCard = document.getElementById("book-card");
+const openFormBtn = document.getElementById("open-form-btn");
+const formContainer = document.getElementById("form-container");
+const bookForm = document.getElementById("book-form");
+
+openFormBtn.addEventListener("click", () => {
+  formContainer.style.display = "block";
+});
+
 const myLibrary = [];
 
 function Book(id, title, author, pages, read) {
@@ -26,3 +35,76 @@ addBookToLibrary("Influence et Manipulation", "Robert B. Cialdini", 230, false);
 addBookToLibrary("Le pouvoir du moment présent", "Eckhart Tolle", 240, true);
 
 console.log(myLibrary);
+
+function displayBook() {
+  bookCard.innerHTML = "";
+
+  myLibrary.forEach((book) => {
+    bookCard.innerHTML += `
+      <div class="book">
+        <div class="card">
+          <div>
+            <h4>Titre </h4>
+            <span>${book.title}</span>
+          </div>
+          <div>
+            <h4>Auteur </h4>
+            <span>${book.author}</span>
+          </div>
+          <div>
+            <h4>Pages </h4>
+            <span>${book.pages}</span>
+          </div>
+          <div>
+            <h4>Lu </h4>
+            <span>${book.read ? "Oui" : "Non"}</span>
+          </div>
+        </div>
+        <div class="update">
+          <button class="add-btn" >Modifier</button>
+          <button class="delete-btn" data-id="${book.id}">Supprimer</button>
+        </div>
+      </div>
+    `;
+  });
+  
+  const deleteButtons = document.querySelectorAll(".delete-btn");
+  deleteButtons.forEach((button) => {
+    button.addEventListener(("click"), () => {
+      const id = button.getAttribute("data-id");
+      const index = myLibrary.findIndex(book => book.id === id);
+      myLibrary.splice(index, 1);
+      displayBook();
+    });
+
+  });
+
+  const addBook = document.querySelectorAll(".add-btn");
+  addBook.forEach((book) => {
+    book.addEventListener(("click"), () => {
+
+    })
+  });
+
+}
+
+displayBook()
+
+bookForm.addEventListener(("submit"), (e) => {
+  e.preventDefault();
+
+  const title = document.getElementById("title").value.trim();
+  const author = document.getElementById("author").value.trim();
+  const page = document.getElementById("pages").value.trim();
+  const read = document.getElementById("read").checked;
+
+  if (title && author && !isNaN(page)) {
+    addBookToLibrary(title, author, page, read);
+    displayBook()
+    bookForm.reset();
+    formContainer.style.display = "none";
+  }else {
+    alert("Veuillez remplir tous les champs correctement.");
+  }
+
+});
